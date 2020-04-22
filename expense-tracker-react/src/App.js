9ace896b64b77ac6182b06051acd52fb9a965184
shawn johnson <shawnjohnson@shawns-MacBook-Air.local>
2020-04-22 13:58:17 -0700

    created a new app

--- expense-tracker-react/src/App.js
+++ expense-tracker-react/src/App.js
@@ -3,6 +3,8 @@ import { Header } from './components/Header';
 import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
@@ -10,14 +12,15 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
---
+++ expense-tracker-react/src/context/GlobalState.js
@@ -0,0 +1,39 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+const initialState = {
+    transactions: [
+        { id: 1, text: 'bleach', amount: -20 },
+        { id: 2, text: 'Salary', amount: 300 },
+        { id: 3, text: 'Mask', amount: -10 },
+        { id: 4, text: 'Insta Cart', amount: 150 }
+    ]
+}
+
+export const GlobalContext = createContext(initialState);
+
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
+
+    return (<GlobalContext.Provider value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
+    }}>
+        {children}
+    </GlobalContext.Provider>);
+}
